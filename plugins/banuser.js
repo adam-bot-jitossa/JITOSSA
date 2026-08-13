c9c5ffff4d7e1654617ca9670aeefab7d87ada3b
next-12 <khadabfest@gmail.com>
2026-08-14 00:02:44 +0100

    Update banuser.js

--- plugins/banuser.js
+++ plugins/banuser.js
@@ -1,15 +1,21 @@
-let handler = async (m, { text }) => {
-	if (!text) throw 'Who wants to be banned?';
-	let who;
-	if (m.isGroup) who = m.mentionedJid[0];
-	else who = m.chat;
-	if (!who) throw 'Tag??';
-	global.db.data.users[who].banned = true;
-	m.reply('Success!');
+let handler = async (m, { conn, text }) => {
+	if (!text) throw '*❌ مـن تـريـد حـظـره؟*\n*مـثـال:*.حـظـر @user';
+	
+	let الشخص;
+	if (m.isGroup) الشخص = m.mentionedJid[0];
+	else الشخص = m.chat;
+	
+	if (!الشخص) throw '*❌ ديـر مـنـشـن لـلـشـخـص*';
+
+	// تأكد ان المستخدم موجود في قاعدة البيانات
+	global.db.data.users[الشخص] = global.db.data.users[الشخص] || {}
+	global.db.data.users[الشخص].banned = true;
+	
+	await conn.reply(m.chat, `*✅ تــم حـظـر الـمـسـتـخـدم* @${الشخص.split('@')[0]} *بـنـجـاح*`, m, { mentions: [الشخص] })
 };
-handler.help = ['ban'];
+handler.help = ['حظر @tag'];
 handler.tags = ['owner'];
-handler.command = /^ban(user)?$/i;
+handler.command = /^(حظر|ban(user)?)$/i;
 handler.owner = true;
 
 export default handler;

e916c2cfa118aa41b8f103ab1df8cab15313f906
next-12 <khadabfest@gmail.com>
2026-08-13 01:52:28 +0100

    Update sticker.js

--- plugins/sticker.js
+++ plugins/sticker.js
@@ -1,27 +1,58 @@
-let handler = async (m, { text }) => {
+// تـرجـمـة وتـعـديـل: نـورديـن
+// بـلـوغـيـن: Izuku-mi | صـنـع سـتـيـكـر مـن صـورة/فـيـديـو
+
+// ─── مـعـلـومـات الـقـنـاة ─────────────────────────────────────────────
+const channelName = '𝙄𝙎𝘼𝙂𝙄 𝙔𝙊𝙄𝘾𝙃𝙄 𝘽𝙊𝙏 - 𝟭𝟭 ⚽⚡'
+const CHANNEL_ID = '120363410733859643@newsletter'
+const newsletter = {
+    forwardingScore: 999,
+    isForwarded: true,
+    forwardedNewsletterMessageInfo: {
+        newsletterJid: CHANNEL_ID,
+        newsletterName: channelName
+    }
+}
+
+let handler = async (m, { conn, text }) => {
 	let q = m.quoted ? m.quoted : m;
 	let mime = (q.msg || q).mimetype || '';
 
 	if (/image|video|webp/.test(mime)) {
 		if ((q.msg?.seconds || q.seconds) > 10) {
-			return m.reply('The video must be under 10 seconds long.');
-		}
+			return conn.sendMessage(m.chat, { 
+				text: '*❌ الـفـيـديـو خـاصـو يـكـون قـل مـن 10 ثـوانـي*',
+				contextInfo: newsletter 
+			}, { quoted: m });
+	}
 
 		let media = await q.download();
-		let exif;
+		
+		let packname = channelName
+		let author = 'CARL-BOT'
 		if (text) {
-			const [packname, author] = text.split(/[,|\-+&]/);
-			exif = { packName: packname || '', packPublish: author || '' };
+			const [p, a] = text.split(/[,|\-+&]/);
+			if(p) packname = p.trim()
+			if(a) author = a.trim()
 		}
-		conn.sendSticker(m.chat, media, m, exif);
+
+		await conn.sendMessage(m.chat, {
+			sticker: media,
+			packname: packname,
+			author: author,
+			contextInfo: newsletter
+	}, { quoted: m });
+
 	} else {
-		m.reply('Send or reply to media to turn it into a sticker.');
+		conn.sendMessage(m.chat, { 
+			text: `*📎 الـرجـاء ارسـال او الـرد عـلـى صـورة/فـيـديـو/سـتـيـكـر لـتـحـويـلـه لـسـتـيـكـر*`,
+			contextInfo: newsletter 
+	}, { quoted: m });
 	}
 };
 
-handler.help = ['sticker'];
-handler.tags = ['sticker'];
-handler.command = /^s(tic?ker)?(gif)?$/i;
+handler.help = ['ستيكر <اسـم الـبـاكـة|الـمـؤلـف>', 'sticker <pack|author>'];
+handler.tags = ['تحويل', 'convert'];
+handler.command = /^(ستيكر|sticker|s|ملصق)$/i;
 handler.register = false;
 
 export default handler;

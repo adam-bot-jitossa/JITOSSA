3093357cdd44ab2e20d09a4dfc0efe65c32b60f5
next-12 <khadabfest@gmail.com>
2026-08-13 02:25:36 +0100

    Update banchat.js

--- plugins/banchat.js
+++ plugins/banchat.js
@@ -1,10 +1,13 @@
 let handler = async (m) => {
+	// تأكد ان الشات موجود في قاعدة البيانات
+	global.db.data.chats[m.chat] = global.db.data.chats[m.chat] || {}
 	global.db.data.chats[m.chat].isBanned = true;
-	m.reply('Done!');
+	
+	await m.reply('*✅ تـم قـفل الـشات بـنجــاح*')
 };
 handler.help = ['banchat'];
 handler.tags = ['owner'];
-handler.command = /^(banchat)$/i;
+handler.command = /^(banchat|قفل_شات)$/i;
 handler.owner = true;
 handler.group = true;
 export default handler;
